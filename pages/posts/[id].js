3e5b87327dbe873f2c5dca5809278887da40b576
James Singleton <jamesrsingleton1@gmail.com>
2020-05-04 20:21:11 -0700

    chore(post): update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,7 +10,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -26,6 +26,7 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
