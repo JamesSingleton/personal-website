df555eb3bc3f415ee2f9b546f7b12e904dee336f
James Singleton <jamesrsingleton1@gmail.com>
2020-10-30 23:04:46 -0700

    chore(resume): remove commented code

--- pages/resume.js
+++ pages/resume.js
@@ -5,11 +5,6 @@ import { NAME, WEBSITE_DESCRIPTION } from '@/lib/constants';
 
 const ResumePage = () => (
   <Page title={`Resume | ${NAME} - ${WEBSITE_DESCRIPTION}`}>
-    {/* <div className="w-full text-center">
-      <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-        Download Resume
-      </button>
-    </div> */}
     <div className="block md:hidden bg-white text-black p-2 mx-2">
       <SmallResume />
     </div>
