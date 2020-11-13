628be6a027f37301274e1c78ed6b11edf7ec238a
James Singleton <jamesrsingleton1@gmail.com>
2020-11-12 21:22:04 -0700

    chore(log): remove console.log

--- pages/about.js
+++ pages/about.js
@@ -6,22 +6,18 @@ import markdownStyles from '@/components/Articles/markdown-styles.module.css'
 
 
 
-const About = ({ data }) =>  {
-  console.log({data})
-  return (
-    <Page title={`About | ${NAME} - ${WEBSITE_DESCRIPTION}`}>
-      <div className="w-full p-4">
-        <h1 className="text-5xl font-extrabold tracking-tighter leading-tight text-gray-100 text-center">{data[0].title}</h1>
-        <div className="max-w-2xl mx-auto">
-          <div className={markdownStyles['markdown']}>
-            {documentToReactComponents(data[0].text)}
-          </div>
+const About = ({ data }) =>  (
+  <Page title={`${data[0].title} | ${NAME} - ${WEBSITE_DESCRIPTION}`}>
+    <div className="w-full p-4">
+      <h1 className="text-5xl font-extrabold tracking-tighter leading-tight text-gray-100 text-center">{data[0].title}</h1>
+      <div className="max-w-2xl mx-auto">
+        <div className={markdownStyles['markdown']}>
+          {documentToReactComponents(data[0].text)}
         </div>
       </div>
-    </Page>
-  )
-};
-
+    </div>
+  </Page>
+);
 
 export async function getServerSideProps(context) {
   const data = await getAllSimpleText()
