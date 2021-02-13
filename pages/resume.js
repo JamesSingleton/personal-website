cef158bbcd5df87f6b25687f536068ca4f3a0509
James Singleton <jamesrsingleton1@gmail.com>
2021-02-13 10:24:19 -0800

    chore(build): fix build error

--- pages/resume.js
+++ pages/resume.js
@@ -1,12 +1,14 @@
-import Page from '@/components/Page';
-import FullResume from '@/components/Resume/FullResume'
-import SmallResume from '@/components/Resume/SmallResume'
-import { NAME, WEBSITE_DESCRIPTION } from '@/lib/constants';
+import Page from "@/components/Layout/Page";
+import FullResume from "@/components/Resume/FullResume";
+import SmallResume from "@/components/Resume/SmallResume";
+import { NAME, WEBSITE_DESCRIPTION } from "@/lib/constants";
 
 const ResumePage = () => (
   <Page title={`Resume | ${NAME} - ${WEBSITE_DESCRIPTION}`}>
     <div className="w-full text-center py-4">
-      <h1 className="text-5xl font-extrabold tracking-tighter leading-tight text-gray-100">Resume</h1>
+      <h1 className="text-5xl font-extrabold tracking-tighter leading-tight text-gray-100">
+        Resume
+      </h1>
     </div>
     <div className="block md:hidden bg-white text-black p-2 mx-2">
       <SmallResume />
